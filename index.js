587368e1433fedc6803b4ea81f5eea0a46bf58d1
Nils Kenneweg <software@ovt.me>
2013-04-17 23:58:19 +0200

    cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-var server = require("./modules/server.js");
-var handler = require("./handler");
-
-/** port to listen on */
-var PORT = 800;
-/** start the server */
-server.start(handler.handle, PORT);
