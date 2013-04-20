aa95d9de61df3650fb3c5e7a3190dd341aea03b5
Nils Kenneweg <nk@ovt.me>
2013-04-20 15:54:56 +0200

    passwordStrength, socket

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(3000);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	console.log("connection received");
+
+	//socket.emit('news', { hello: 'world' });
+	
+	socket.on('login', function (data, fn) {
+		console.log(data);
+		fn({result: "success"});
+	});
+	
+	socket.on('disconnect', function () {
+		console.log("client disconnected");
+	});
 });
