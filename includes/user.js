4ff53c0cb588c80da5b0988c845f728e645b20c1
Nils Kenneweg <software@ovt.me>
2013-05-02 23:16:28 +0200

    user started. WITH REDIS :)

--- includes/user.js
+++ includes/user.js
@@ -1,15 +1,42 @@
 "use strict";
 
 var step = require("step");
+var redis = require("redis");
+var h = require("./helper");
 
-var User = function () {
+function isMail(mail) {
+
+}
+
+function isNickName(nickname) {
+
+}
+
+var User = function (id) {
 
 };
 
 User.getUser = function (identifier, callback) {
+	var theClient;
 	step(function () {
-		throw new UserNotExisting(identifier);
-	}, callback);
+		theClient = redis.createClient();
+
+		if (h.isMail(identifier)) {
+			theClient.get("user:mail:" + identifier, this);
+		} else if (h.isNickname(identifier)) {
+			theClient.get("user:nickname:" + identifier, this);
+		} else if (h.isID(identifier)) {
+			this.ne(parseInt(identifier, 10));
+		} else {
+			throw new UserNotExisting(identifier);
+		}
+	}, h.sF(function (id) {
+		if (id) {
+			return new User(id);
+		} else {
+			throw new UserNotExisting(identifier);
+		}
+	}));
 };
 
 module.exports = User;
