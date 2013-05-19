ae0aac0ddd762afbcd507c7eb2120c69593b45d7
Nils Kenneweg <software@ovt.me>
2013-05-19 14:35:51 +0200

    elliptic curve key added.

---
+++ includes/crypto/eccKey.js
@@ -0,0 +1,96 @@
+var step = require("step");
+var client = require("../client");
+var h = require("../helper");
+
+require("../errors");
+
+"use strict";
+
+var EccKey = function (keyRealID) {
+	var theKey = this;
+
+	/** getter for keyRealID */
+	this.getRealID = function getRealIDF() {
+		return keyRealID;
+	};
+
+	this.getCurve = function getCurveF(cb) {
+		step(function () {
+			client.get("key:" + keyRealID + ":curve", this);
+		}, cb);
+	};
+
+	this.getPoint = function getPointF(cb) {
+		step(function () {
+			theKey.getPointX(this.parallel());
+			theKey.getPointY(this.parallel());
+		}, h.sF(function (data) {
+			this.ne({
+				x: data[0],
+				y: data[1]
+			});
+		}), cb);
+	};
+
+	this.getPointX = function getPointXF(cb) {
+		step(function () {
+			client.get("key:" + keyRealID + ":point:x", this);
+		}, cb);
+	};
+
+	this.getPointY = function getPointYF(cb) {
+		step(function () {
+			client.get("key:" + keyRealID + ":point:y", this);
+		}, cb);
+	};
+
+	this.getDecryptors = function getDecryptorsF(cb) {
+		step(function () {
+			var Decryptor = require("./decryptor");
+			Decryptor.getAll(keyRealID, this);
+		}, cb);
+	};
+
+	this.addDecryptor = function addDecryptorF(data, cb) {
+		step(function () {
+			var Decryptor = require("./decryptor");
+			Decryptor.create(keyRealID, data, this);
+		}, cb);
+	};
+};
+
+/** get all decryptors for a certain key id */
+EccKey.get = function getF(keyRealID, cb) {
+	//TODO check keyRealID is a valid keyRealID!
+	step(function () {
+		client.get("key:" + keyRealID, this);
+	}, h.sF(function (keyData) {
+		if (keyData === "ecckey") {
+			this.ne(new EccKey(keyRealID));
+		} else {
+			throw new NotAEccKey();
+		}
+	}), cb);
+};
+
+/** create a symmetric key */
+EccKey.create = function (keyRealID, data, cb) {
+	//TODO: check keyRealID for correctness
+	step(function () {
+		client.setnx("key:" + keyRealID, "ecckey", this);
+	}, h.sF(function (data) {
+		if (data === 0) {
+			throw new RealIDInUse();
+		}
+
+		if (!data || !data.curve || !data.point || !data.point.x || !data.point.y || !h.isHex(data.point.x) || !h.isHex(data.point.y) || !h.isCurve(data.curve)) {
+			throw new InvalidEccKey("Missing data");
+		}
+
+		client.set("key:" + keyRealID + ":curve", data.curve, this.parallel());
+		client.set("key:" + keyRealID + ":point:x", data.point.x, this.parallel());
+		client.set("key:" + keyRealID + ":point:y", data.point.y, this.parallel());
+
+		this.ne(new EccKey(keyRealID));
+	}), cb);
+};
