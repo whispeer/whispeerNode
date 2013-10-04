f46939eb6d1487f870302d2ab15f0be8ef2fa4b4
Nils Kenneweg <software@ovt.me>
2013-10-04 23:39:27 +0200

    emptyRedis error handling.

--- emptyRedis.js
+++ emptyRedis.js
@@ -1,7 +1,11 @@
 "use strict";
 var client = require("./includes/redisClient");
 
-client.flushdb(function () {
-	console.log("done");
+client.flushdb(function (e) {
+	if (e) {
+		console.error("did not empty database!");
+	} else {
+		console.log("done");
+	}
 	process.exit();
 });
