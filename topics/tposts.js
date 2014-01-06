c3b0a6e8cdfb213f2030b5efb98565d778e13837
Nils Kenneweg <software@ovt.me>
2014-01-07 00:50:47 +0100

    write all api calls for posts.

--- topics/tposts.js
+++ topics/tposts.js
@@ -31,8 +31,47 @@ var p = {
 			});
 		}), fn);
 	},
-	getTimeline: function (data, fn, view) {},
-	getWall: function (data, fn, view) {}
+	getTimeline: function (data, fn, view) {
+		step(function () {
+			Post.getTimeline(view, data.filter, data.start, data.count, this);
+		}, h.sF(function (posts) {
+			var i;
+			for (i = 0; i < posts.length; i += 1) {
+				posts[i].getPostData(view, this.parallel(), data.addKey);
+			}
+		}), h.sF(function (data) {
+			this.ne({
+				posts: data
+				//TODO: remaining: false|true
+			});
+		}), fn);
+	},
+	getWall: function (data, fn, view) {
+		step(function () {
+			Post.getUserWall(view, data.userid, data.start, data.count, this);
+		}, h.sF(function (posts) {
+			var i;
+			for (i = 0; i < posts.length; i += 1) {
+				posts[i].getPostData(view, this.parallel(), data.addKey);
+			}
+		}), h.sF(function () {
+			this.ne({
+				posts: data
+				//TODO: remaining: false|true
+			});
+		}), fn);
+	},
+	createPost: function (data, fn, view) {
+		step(function () {
+			Post.create(view, data.postData, this);
+		}, h.sF(function (thePost) {
+			thePost.getPostData(view, this);
+		}), h.sF(function (data) {
+			this.ne({
+				createdPost: data
+			});
+		}), fn);
+	}
 };
 
 module.exports = p;
