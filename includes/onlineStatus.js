9f8ffbdc6500521bfc3abb3b225b2f40ee22b749
Nils Kenneweg <nk@ovt.me>
2014-04-01 13:22:39 +0200

    notify if someone goes away/goes back online.

--- includes/onlineStatus.js
+++ includes/onlineStatus.js
@@ -5,8 +5,28 @@ var h = require("whispeerHelper");
 
 var friends = require("./friends");
 
+var awayTimeout = 30*1000;
+
+setInterval(function () {
+	var time = new Date().getTime();
+	step(function () {
+		client.zrangebyscore("user:awayCheck", "-inf", time, this);
+	}, h.sF(function (awayUsers) {
+		console.log("remove: " + awayUsers.length);
+		awayUsers.map(function (userid) {
+			friends.notifyUsersFriends(userid, "online", 1);
+		});
+
+		client.zremrangebyscore("user:awayCheck", "-inf", time, this);
+	}), function (e) {
+		console.error(e);
+	});
+
+}, 10*1000);
+
+
 function onlineStatusUpdater(view, session) {
-	var userid;
+	var userid, timeout;
 
 	function removeSocket() {
 		console.log("remove socket " + view.getSocket().id + " from user " + userid);
@@ -14,6 +34,7 @@ function onlineStatusUpdater(view, session) {
 		if (userid) {
 			var socketID =  view.getSocket().id;
 			var userIDToRemove = userid;
+			timeout = 0;
 
 			userid = 0;
 
@@ -25,7 +46,10 @@ function onlineStatusUpdater(view, session) {
 				if (count > 0) {
 					this.ne();
 				} else {
-					client.srem("user:online", userIDToRemove, this);
+					client.multi()
+						.srem("user:online", userIDToRemove)
+						.zrem("user:awayCheck", userIDToRemove)
+						.exec(this);
 					friends.notifyUsersFriends(userIDToRemove, "online", 0);
 					this.ne();
 				}
@@ -37,6 +61,7 @@ function onlineStatusUpdater(view, session) {
 
 	function addSocket() {
 		userid = view.getUserID();
+		var alreadyNotified = false;
 
 		console.log("added socket " + view.getSocket().id + " from user " + userid);
 
@@ -48,12 +73,18 @@ function onlineStatusUpdater(view, session) {
 				}
 
 				if (added) {
+					alreadyNotified = true;
 					friends.notifyAllFriends(view, "online", 2);
 				}
 			})
 			.sadd("user:" + userid + ":sockets", view.getSocket().id)
-			.set("user:" + userid + ":recentActivity", "1")
-			.expire("user:" + userid + ":recentActivity", 10*60)
+			.getset("user:" + userid + ":recentActivity", "1", function (oldValue) {
+				if (!oldValue && !alreadyNotified) {
+					friends.notifyAllFriends(view, "online", 2);
+				}
+			})
+			.zadd("user:awayCheck", new Date().getTime() + awayTimeout, userid)
+			.expire("user:" + userid + ":recentActivity", awayTimeout)
 			.exec(function (e) {
 				if (e) {
 					console.error(e);
