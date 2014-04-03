ec72d3766d61434f3c8ad29c0242a3dd7b3d105a
Nils Kenneweg <software@ovt.me>
2014-04-03 15:54:38 +0200

    get version string from changelog.

--- webserver.js
+++ webserver.js
@@ -31,7 +31,7 @@ app2.listen(8088);
 
 var app = express();
 
-var directoryParts = ["assets", "favicon.png", "favicon.ico"];
+var directoryParts = ["assets", "favicon.png", "favicon.ico", "changelog.json"];
 var angular = ["user", "messages", "circles", "main", "friends", "login"];
 
 app.use(function(req, res, next) {
