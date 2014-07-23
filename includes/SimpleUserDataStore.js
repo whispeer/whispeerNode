a034452436bcdf8858c0339e44042b7664f4cb45
Nils Kenneweg <nk@ovt.me>
2014-07-23 16:10:45 +0200

    add signaturecache.

--- includes/SimpleUserDataStore.js
+++ includes/SimpleUserDataStore.js
@@ -17,16 +17,18 @@ function SimpleUserDataStore(name) {
 }
 
 SimpleUserDataStore.prototype.get = function (view, cb) {
+	var that = this;
 	step(function () {
-		client.get("user:" + view.getUserID() + ":" + this._name, this);
+		client.get("user:" + view.getUserID() + ":" + that._name, this);
 	}, h.sF(function (result) {
 		this.ne(JSON.parse(result));
 	}), cb);
 };
 
 SimpleUserDataStore.prototype.set = function (view, newContent, cb) {
+	var that = this;
 	step(function () {
-		client.set("user:" + view.getUserID() + ":" + this._name, JSON.stringify(newContent), this);
+		client.set("user:" + view.getUserID() + ":" + that._name, JSON.stringify(newContent), this);
 	}, h.sF(function (res) {
 		this.ne(res === "OK");
 	}), cb);
--- topics.js
+++ topics.js
@@ -152,6 +152,14 @@ var whispeerAPI = {
 			}), fn);
 		}
 	},
+	signatureCache: {
+		get: function (data, fn, view) {
+			new SimpleUserDataStore("signatureCache").get(view, h.objectifyResult("content", fn));
+		},
+		set: function (data, fn, view) {
+			new SimpleUserDataStore("signatureCache").set(view, data.content, h.objectifyResult("success", fn));
+		}
+	},
 	trustManager: {
 		get: function (data, fn, view) {
 			new SimpleUserDataStore("trustManager").get(view, h.objectifyResult("content", fn));
