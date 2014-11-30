01e5207276f8c7517d441765193c1dae1053d62b
Martin Czuchra <martin.czuchra@googlemail.com>
2014-11-30 16:08:39 +0100

    Only the actual code should go to stdout, the rest is stderr.

--- generateInviteCode.js
+++ generateInviteCode.js
@@ -2,6 +2,7 @@
 
 "use strict";
 
+var util = require("util");
 var invites = require("./includes/invites");
 
 var viewStub = {
@@ -9,22 +10,25 @@ var viewStub = {
 	getUserID: function () { return -1; }
 };
 
+
 var configManager = require("./includes/configManager");
 var config = configManager.get();
 var client = require("./includes/redisClient");
 
 client.select(config.dbNumber || 0, function (e) {
+
 	if (e) {
 		throw e;
 	}
-	console.log("Database selected: " + config.dbNumber || 0);
+
+	util.error(util.format("Database selected: %d", config.dbNumber || 0));
 
 	invites.generateCode(viewStub, function (e, c) {
 		if (e) {
 			throw e;
 		}
 
-		console.log(c);
+		util.puts(c);
 		process.exit();
 	});
 });
