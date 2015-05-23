86497e1e6339f064b4cb8754febedae5d0c53b22
Nils Kenneweg <nk@ovt.me>
2015-05-23 14:21:47 +0200

    show if a userid has no nickname.

--- migrations/00002-nicknameLowerCase.js
+++ migrations/00002-nicknameLowerCase.js
@@ -6,15 +6,23 @@ var h = require("whispeerHelper");
 var client = require("../includes/redisClient");
 
 function updateUserNicknames(cb) {
+	var userids;
+
 	step(function () {
 		client.smembers("user:list", this);
-	}, h.sF(function (userids) {
+	}, h.sF(function (_userids) {
+		userids = _userids;
 		userids.forEach(function (userid) {
 			client.hget("user:" + userid, "nickname", this.parallel());
 		}, this);
 	}), h.sF(function (nicknames) {
 		var nicknamesHashMap = {};
-		nicknames.forEach(function (nickname) {
+		nicknames.forEach(function (nickname, i) {
+			if (!nickname) {
+				console.log("no nickname for " + userids[i]);
+				return;
+			}
+
 			var nick = nickname.toLowerCase();
 
 			if (!nicknamesHashMap[nick]) {
