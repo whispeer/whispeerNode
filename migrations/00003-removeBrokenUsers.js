d9f1721777873cc114887cdc93e0213b6d25f55d
Nils Kenneweg <nk@ovt.me>
2015-05-26 23:27:59 +0200

    new migration 3.

---
+++ migrations/00003-removeBrokenUsers.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var step = require("step");
+var h = require("whispeerHelper");
+
+var client = require("../includes/redisClient");
+
+function deleteUser(id, cb) {
+	if (h.parseDecimal(id) > 0) {
+		step(function () {
+			client.keys("user:" + id, this);
+		}, h.sF(function (keys) {
+			console.log(keys);
+		}));
+	} else {
+		console.log("that is not an id: " + id);
+		cb();
+	}
+}
+
+function updateUserNicknames(cb) {
+	var userids;
+
+	step(function () {
+		client.smembers("user:list", this);
+	}, h.sF(function (_userids) {
+		userids = _userids;
+		userids.forEach(function (userid) {
+			client.hget("user:" + userid, "nickname", this.parallel());
+		}, this);
+	}), h.sF(function (nicknames) {
+		nicknames.map(function (nickname, i) {
+			return {
+				nickname: nickname,
+				id: userids[i]
+			};
+		}).filter(function (user) {
+			return !user.nickname;
+		}).forEach(function (user) {
+			deleteUser(user.id, this.parallel());
+		}, this);
+	}), cb);
+}
+
+module.exports = updateUserNicknames;
