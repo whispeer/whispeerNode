a0465109bd111c5beeff406c97fe9cd02fe08626
Daniel Melchior <d.melchior@me.com>
2015-07-08 19:32:54 +0200

    new mail templates

---
+++ webserver.js
@@ -0,0 +1,89 @@
+"use strict";
+
+var express = require("express");
+var helmet = require("helmet");
+
+var APP_PORT        = process.env.WHISPEER_PORT            || 80;
+var APP2_PORT       = process.env.WHISPEER_APP2_PORT       || 8088;
+var NEWSLETTER_PORT = process.env.WHISPEER_NEWSLETTER_PORT || 8089;
+var STYLEGUIDE_PORT = process.env.WHISPEER_STYLEGUIDE_PORT || 8090;
+
+//default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self'
+
+var policy = {
+  defaultPolicy: {
+    "default-src": ["'self'", "ws://127.0.0.1:3000", "http://127.0.0.1:3000", "ws://192.168.0.4:3000", "http://192.168.0.4:3000", "http://192.168.178.40:3000", "ws://192.168.178.40:3000"],
+    "style-src": ["'self'", "'unsafe-inline'"],
+    "script-src": ["'self'"],
+    "img-src": ["'self'", "data:", "blob:"]
+  }
+};
+
+helmet.csp.policy(policy);
+
+var app2 = express();
+
+app2.use(express.methodOverride());
+app2.use(express.bodyParser());
+app2.use(helmet.csp());
+app2.use(helmet.xframe());
+app2.use(helmet.iexss());
+app2.use(helmet.cacheControl());
+
+app2.use(express.static("../app2directory-build/"));
+
+app2.listen(APP2_PORT);
+
+var app = express();
+
+var directoryParts = ["assets", "favicon.png", "favicon.ico", "changelog.json"];
+var angular = ["user", "messages", "circles", "main", "friends", "login"];
+
+app.use(function(req, res, next) {
+  var urlParts = req.url.split(/\/|\?/);
+
+  if (directoryParts.indexOf(urlParts[1]) === -1) {
+    if (angular.indexOf(urlParts[1]) === -1) {
+      console.log(req.url);
+    }
+    req.url = "/";
+  }
+
+  next();
+});
+
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+//app.use(helmet.csp());
+app.use(helmet.xframe());
+app.use(helmet.iexss());
+app.use(helmet.cacheControl());
+
+app.use(express.static("../whispeerFrontend/"));
+
+app.listen(APP_PORT);
+
+var newsletter = express();
+
+newsletter.use(express.methodOverride());
+newsletter.use(express.bodyParser());
+newsletter.use(helmet.xframe());
+newsletter.use(helmet.iexss());
+newsletter.use(helmet.cacheControl());
+
+newsletter.use(express.static("../newsletter/"));
+
+newsletter.listen(NEWSLETTER_PORT);
+
+var styleguide = express();
+
+styleguide.use(express.methodOverride());
+styleguide.use(express.bodyParser());
+styleguide.use(helmet.xframe());
+styleguide.use(helmet.iexss());
+styleguide.use(helmet.cacheControl());
+
+styleguide.use(express.static("../styleguide/"));
+
+styleguide.listen(STYLEGUIDE_PORT);
+
