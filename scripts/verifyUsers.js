f28d23a0262fb70e8e789846f6e2cad4b8d290b3
Nils Kenneweg <nk@ovt.me>
2015-07-17 14:10:50 +0200

    add a script to verify all users (trustManager and settings for now)

---
+++ scripts/verifyUsers.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var setup = require("../includes/setup");
+var client = require("../includes/redisClient");
+var h = require("whispeerHelper");
+var User = require("../includes/user");
+
+var Bluebird = require("bluebird");
+
+var setupP = Bluebird.promisify(setup);
+
+var settings = require("../includes/settings");
+var getUser = Bluebird.promisify(User.getUser, User);
+var SimpleUserDataStore = require("../includes/SimpleUserDataStore");
+var trustManager = new SimpleUserDataStore("trustManager");
+
+var getOwnSettings = Bluebird.promisify(settings.getOwnSettings, settings);
+var getTrustManager = Bluebird.promisify(trustManager.get, trustManager);
+
+var verifySecuredMeta = Bluebird.promisify(require("../includes/verifyObject"));
+
+function verifySettings(request) {
+	return getOwnSettings(request).then(function (settings) {
+		if (settings.meta) {
+			return verifySecuredMeta(request, settings.meta, "settings");
+		}
+	}).catch(function () {
+		console.log("Broken settings: " + request.session.getUserID());
+	});
+}
+
+function verifyTrustManager(request) {
+	return getTrustManager(request).then(function (trustManager) {
+		return verifySecuredMeta(request, trustManager, "trustManager");
+	}).catch(function () {
+		console.log("Broken trustManager: " + request.session.getUserID());
+	});
+}
+
+function verifyUser(userID) {
+	var requestStub = {
+		session: {
+			logedinError: function (cb) { cb(); },
+			getUserID: function () { return h.parseDecimal(userID); }
+		}
+	};
+
+	return getUser(userID).then(function (theUser) {
+		return Bluebird.all([
+			verifySettings(requestStub, theUser),
+			verifyTrustManager(requestStub, theUser)
+		]);
+		//get settings
+		//get trustManager
+		//get signedKeys
+	});
+}
+
+
+setupP().then(function () {
+	return client.smembersAsync("user:list");
+}).map(function (userID) {
+	return verifyUser(userID);
+}).then(function () {
+	process.exit();
+});
