19d5eaed4d3c1dd5fc093d7cfa57cd0d7fbbee45
Nils Kenneweg <nk@ovt.me>
2017-03-06 15:30:35 +0100

    start work on migration

---
+++ migrations/00010-postgres-pushToken.js
@@ -0,0 +1,44 @@
+var Bluebird = require("bluebird");
+var	client = require("../includes/redisClient");
+
+const sequelize = require("../includes/dbConnector/sequelizeClient");
+const pushTokenModel = require("../includes/models/pushTokenModelSequelize");
+
+const getAllPushTokenIDs = () => {
+	"use strict";
+
+	return client.smembersAsync("waterline:pushtoken:id");
+};
+
+const getPushToken = (id) => {
+	"use strict";
+
+	return client.getAsync(id).then((pushToken) => {
+		return JSON.parse(pushToken);
+	});
+};
+
+function addTopicUpdatesToPostgres(cb) {
+	"use strict";
+	return Bluebird.resolve().then(function () {
+		return sequelize.sync();
+	}).then(() => {
+		return getAllPushTokenIDs();
+	}).map(function (pushTokenID) {
+		console.log("id", pushTokenID);
+		return getPushToken(pushTokenID);
+	}).then((pushTokens) => {
+		console.log(pushTokens);
+		return new Bluebird(function () {
+			
+		});
+		
+		return sequelize.transaction((transaction) => {
+			return Bluebird.resolve(pushTokens).map((pushToken) => {
+				return pushTokenModel.create(pushToken, {transaction: transaction});
+			});
+		});
+	}).nodeify(cb);
+}
+
+module.exports = addTopicUpdatesToPostgres;
