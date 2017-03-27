d493ee7c55859c54e9dc73602a4c39338e4c74b6
Nils Kenneweg <nk@ovt.me>
2017-03-27 19:01:46 +0200

    get user name for push of request

--- includes/friends.js
+++ includes/friends.js
@@ -12,11 +12,23 @@ var SymKey = require("./crypto/symKey");
 var verifySecuredMeta = require("./verifyObject");
 var pushAPI = require("./pushAPI");
 
-function pushFriendRequest(senderId, receiverId) {
-	pushAPI.notifyUser(receiverId, {}, {
-		type: "contactRequest",
-		id: senderId
-	});
+function pushFriendRequest(request, senderId, receiverId) {
+	step(function () {
+		var User = require("./user");
+
+		User.getUser(senderId, this);
+	}, h.sF(function (sender) {
+		sender.getNames(request, this);
+	}), h.sF(function (senderNames) {
+		var senderName = senderNames.firstName || senderNames.lastName || senderNames.nickname;
+		
+		pushAPI.notifyUser(receiverId, {
+			user: senderName
+		}, {
+			type: "contactRequest",
+			id: senderId
+		});
+	}));
 }
 
 var Notification = require("./notification");
@@ -444,7 +456,7 @@ var friends = {
 					sendMailWhileOnline: true
 				});
 
-				pushFriendRequest(request.session.getUserID(), friendShip.user);
+				pushFriendRequest(request, request.session.getUserID(), friendShip.user);
 				friendShip.user.notify("friendRequest", request.session.getUserID());
 			} else {
 				Notification.add([friendShip.user], "friend", "accept", request.session.getUserID(), {
