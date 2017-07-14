0f248879f22a19567f7e6a1de32d45ee7e026771
Nils Kenneweg <nk@ovt.me>
2017-07-14 13:00:41 +0200

    slow query log

--- includes/dbConnector/sequelizeClient.js
+++ includes/dbConnector/sequelizeClient.js
@@ -4,5 +4,13 @@ const Sequelize = require("sequelize");
 
 module.exports = new Sequelize(
 	process.env.WHISPEERDB ||
-	"postgres://whispeer:whispeer@localhost/whispeer"
-);
+	"postgres://whispeer:whispeer@localhost/whispeer",
+{
+	benchmark: true,
+	logging: (query, time) => {
+		if (time > 50) {
+			// eslint-disable-next-line no-console
+			console.warn(`Slow query (${time}): ${query.replace(/\s+/g, " ").replace(/\n/g, "")}`)
+		}
+	}
+});
