c4c3c7afa6eec4411ace9cfad8e4f163abac33a2
Nils Kenneweg <nk@ovt.me>
2017-11-17 11:05:12 +0100

    remove old mail handlers in favor of sentry. add context to sentry

--- includes/errorService.js
+++ includes/errorService.js
@@ -5,50 +5,17 @@ const Raven = require("raven");
 var errorService = {
 	handleError: function (e, request) {
 		if (e) {
+			const headers = request.socket.handshake.headers
+			const { channel, rawRequest } = request
+
+			Raven.setUserContext({
+				user: request.session.getUserID(),
+				channel,
+				rawRequest,
+				headers
+			})
+
 			Raven.captureException(e);
-			try {
-				console.error(e);
-
-				if (e.id === 12) {
-					return
-				}
-
-				var error = "Unknown Error";
-				var stack = e.stack;
-
-				if (!e.stack) {
-					//lets get a stack from here on
-					try {
-						throw new Error(error);
-					} catch (e) {
-						stack = e.stack;
-					}
-				}
-
-				error = stack.split("\n")[0];
-
-				var errString;
-				try {
-					errString = JSON.stringify(e);
-				} catch (e) {
-					errString = e.toString();
-				}
-
-				var requestString = "";
-
-				if (request) {
-					requestString += "\n\n";
-					requestString += "User: " + request.session.getUserID() + "\n";
-					requestString += "Channel: " + request.channel + "\n";
-					requestString += "Raw Request: " + JSON.stringify(request.rawRequest) + "\n";
-					requestString += "Headers: " + JSON.stringify(request.socket.handshake.headers);
-				}
-
-				var mailer = require("./mailer");
-				mailer.mailAdmin("An Error occured (" + (e.type || error.substr(0, 70)) + ")", errString + "\n" + e.stack + requestString);
-			} catch (e) {
-				console.error(e);
-			}
 		}
 	}
 };
