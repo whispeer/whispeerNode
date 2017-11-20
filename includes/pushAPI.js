7c8db6e9c8d81b560f28c30b33f2065eb3a2af66
Nils Kenneweg <nk@ovt.me>
2017-11-20 14:18:07 +0100

    send error if push is large for debugging

--- includes/pushAPI.js
+++ includes/pushAPI.js
@@ -142,6 +142,10 @@ const pushToken = sequelize.define("pushToken", {
 			}
 
 			if (this.deviceType === "ios") {
+				if (JSON.stringify(payload).length > 2000) {
+					errorService.handleError(new Error(`large push ${JSON.stringify(data)}`))
+				}
+
 				return pushService.pushIOSData(this.token, payload, this.sandbox);
 			}
 
