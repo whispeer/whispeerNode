15d70882e9db8e8fac1bed688a44a6af0cdd871e
Nils Kenneweg <nk@ovt.me>
2017-11-23 13:31:33 +0100

    missing signed keys

---
+++ scripts/missingSignedKeys.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const setup = require("../includes/setup");
+const client = require("../includes/redisClient");
+
+const helper = require("whispeerHelper")
+
+const Bluebird = require("bluebird");
+Bluebird.longStackTraces();
+
+const setupP = Bluebird.promisify(setup);
+
+const checkSignedList = (signedList) => {
+	return client.hgetallAsync(signedList)
+		.then((entry) =>
+			Object.keys(entry)
+				.map((k) => entry[k])
+				.filter((key) => helper.isRealID(key))
+		)
+		.filter((key) => client.existsAsync(`key:${key}`).then((b) => !b))
+		.then((missing) => console.log(missing))
+}
+
+Bluebird.try(() => {
+	return setupP();
+}).then(() => {
+	return client.keysAsync("friends:*:signedList")
+}).map((signedList) => {
+	return checkSignedList(signedList)
+}).then(() => {
+	process.exit(0)
+})
