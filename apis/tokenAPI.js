355687cf8ec789f4e9d560d75e282bf1809a03c4
Nils Kenneweg <nk@ovt.me>
2017-11-30 14:56:54 +0100

    finalize token code

--- apis/tokenAPI.js
+++ apis/tokenAPI.js
@@ -1,10 +1,6 @@
 "use strict"
 
-const Bluebird = require("bluebird")
-
 const CompanyToken = require("../includes/models/companyToken")
-const CompanyUser = require("../includes/models/companyUser")
-const sequelize = require("../includes/dbConnector/sequelizeClient");
 
 module.exports = {
 	get: ({ token }, fn) => {
@@ -20,34 +16,8 @@ module.exports = {
 	},
 	use: ({ token }, fn, request) => {
 		return request.session.logedinError()
-			.then(() => {
-				return sequelize.transaction((transaction) => {
-					return CompanyToken.findOne({ where: { token }}, { transaction })
-						.then((dbToken) => {
-							if (!dbToken) {
-								throw new Error("Token invalid")
-							}
-
-							const { uses, used, CompanyId } = dbToken
-
-							if (uses <= used) {
-								throw new Error("Token invalid")
-							}
-
-							const userID = request.session.getUserID()
-
-							console.log(`add ${userID} to company ${CompanyId} via token ${token}`)
-
-							dbToken.used = dbToken.used + 1
-
-							return sequelize.transaction((transaction) => {
-								return Bluebird.all([
-									CompanyUser.create({ userID, CompanyId }, { transaction }),
-									dbToken.save({ transaction })
-								])
-							})
-						})
-				})
-			}).nodeify(fn)
+			.then(() => CompanyToken.use(token, request.session.getUserID()) )
+			.then(() => ({}))
+			.nodeify(fn)
 	},
 }
--- includes/models/companyToken.js
+++ includes/models/companyToken.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const Bluebird = require("bluebird")
+
 const sequelize = require("../dbConnector/sequelizeClient")
 
 const Company = require("./company")
+const CompanyUser = require("./companyUser")
 
 const {
 	autoIncrementInteger,
@@ -21,6 +24,34 @@ const CompanyToken = sequelize.define("CompanyToken", {
 	uses: required(integer()),
 	used: required(integer()),
 })
+
+CompanyToken.use = (token, userID) => {
+	return sequelize.transaction((transaction) => {
+		return CompanyToken.findOne({ where: { token }}, { transaction })
+			.then((dbToken) => {
+				if (!dbToken) {
+					throw new Error("Token invalid")
+				}
+
+				const { uses, used, CompanyId } = dbToken
+
+				if (uses <= used) {
+					throw new Error("Token invalid")
+				}
+
+				console.log(`add ${userID} to company ${CompanyId} via token ${token}`)
+
+				dbToken.used = dbToken.used + 1
+
+				return sequelize.transaction((transaction) => {
+					return Bluebird.all([
+						CompanyUser.create({ userID, CompanyId }, { transaction }),
+						dbToken.save({ transaction })
+					])
+				})
+			})
+	})
+}
 
 hasMany(Company, CompanyToken)
 
