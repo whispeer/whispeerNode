531e5a0dda5a03fb643fb6ea404db47a55233d97
Nils Kenneweg <nk@ovt.me>
2017-11-30 15:42:28 +0100

    migration for companies to psql

---
+++ migrations/00011-postgres-companies.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const Bluebird = require("bluebird");
+const client = require("../includes/redisClient");
+
+const sequelize = require("../includes/dbConnector/sequelizeClient");
+require("../includes/models/companyUser");
+const Company = require("../includes/models/company");
+
+const createCompany = (company) =>
+	Company.create(company, {
+		include: [{
+			association: Company.CompanyUser,
+		}]
+	})
+
+const getAllCompanies = () =>
+	client.smembersAsync("user:list")
+		.map((userID) => client.smembersAsync(`user:${userID}:companies`)
+			.then((companies) => ({ companies, userID }))
+		)
+		.filter((info) => info.companies.length > 0)
+		.then((userCompanies) => {
+			const companies = {}
+
+			userCompanies.forEach((info) => {
+				info.companies.forEach((name) => {
+					if (!companies[name]) {
+						companies[name] = { companyUser: [], name, licenses: 5000, trial: true }
+					}
+
+					companies[name].companyUser.push({ userID: info.userID })
+				})
+			})
+
+			return Object.keys(companies).map((key) => companies[key])
+		})
+
+
+function addCompaniesToPostgres(cb) {
+	return Bluebird.resolve().then(() =>
+		sequelize.sync()
+	).then(() =>
+		getAllCompanies()
+	).map((company) =>
+		createCompany(company)
+	).then(() =>
+		new Bluebird(() => {})
+	).nodeify(cb);
+}
+
+module.exports = addCompaniesToPostgres;
